090ef42079abe80a522fc5a135e0981bfd28f2d3
kseniya <kseniyamudrakovadev@gmail.com>
2022-01-16 00:21:42 +0100

    final index

--- index.js
+++ index.js
@@ -9,27 +9,32 @@ const server = http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-Type': 'text/plain'
     });
-    res.write('Hello World!');
     res.end();
 });
 
-app.get("/", (req, res) => {
+app.get("/index.html", (req, res) => {
     res.sendFile('/home.html', {
         root: __dirname
     })
 });
 
-app.get("/about", (req, res) => {
+app.get("/about.html", (req, res) => {
     res.sendFile('/about', {
         root: __dirname
     })
 });
 
-app.get("/contact-me", (req, res) => {
+app.get("/contact-me.html", (req, res) => {
     res.sendFile('./about-me.html', {
         root: __dirname
     })
-})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile('./404.html', {
+        root: __dirname
+    })
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
