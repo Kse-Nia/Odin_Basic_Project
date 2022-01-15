4ae91aeaca2edf939ad57e22e99fc6dc46ba2c7c
kseniya <kseniyamudrakovadev@gmail.com>
2022-01-15 19:25:09 +0100

    add files html

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require("express");
 require('dotenv').config();
 const http = require("http");
 const port = process.env.PORT;
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write('Hello World!');
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
